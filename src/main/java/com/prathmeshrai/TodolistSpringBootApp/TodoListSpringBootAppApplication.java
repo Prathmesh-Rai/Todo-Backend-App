dc544675cf78ca4fed203baca0c3d1156a8a2d8d
Prathmesh <surajrai041999@gmail.com>
2024-03-18 14:18:51 +0530

    First Commit-Project Created

---
+++ src/main/java/com/prathmeshrai/TodolistSpringBootApp/TodoListSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.prathmeshrai.TodolistSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListSpringBootAppApplication.class, args);
+	}
+
+}
